3ccb068197759430c2ab0bb026a2b90abd92685e
Mario Leal <mario.lealgarcia@alum.uca.es>
2024-03-20 22:30:50 +0100

    Obstaculos

--- Assets/Script/Movimiento.cs
+++ Assets/Script/Movimiento.cs
@@ -10,6 +10,9 @@ public class Movimiento : MonoBehaviour
     public Vector3 posObjetivo;
     public float velocidad=18;
     public Mundo mundo;
+    public Transform grafico;
+    public LayerMask capaObstaculos;
+    public float distanciaVista=1;
     // Start is called before the first frame update
     void Start()
     {
@@ -32,12 +35,23 @@ public class Movimiento : MonoBehaviour
             MoverLados(1);
         }
     }
+    private void OnDrawGizmos()
+    {
+        Ray rayo = new Ray(grafico.position + Vector3.one * 0.5f,grafico.forward);
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(grafico.position + Vector3.up * 0.5f,grafico.position + Vector3.up * 0.5f + grafico.forward * distanciaVista);
+    }
     public void ActualizarPosicion(){
         posObjetivo =new Vector3(lateral,0,posicionZ);
         transform.position =Vector3.Lerp(transform.position,posObjetivo,velocidad*Time.deltaTime);
     }
     public void Avanzar()
     {
+        grafico.eulerAngles = new Vector3(0,0,0);
+        if(MirarAdelante())
+        {
+            return;
+        }
         posicionZ++;
         if (posicionZ > carril)
         {
@@ -47,13 +61,34 @@ public class Movimiento : MonoBehaviour
     }
     public void Retroceder()
     {
+        grafico.eulerAngles = new Vector3(0,180,0);
+        if(MirarAdelante())
+        {
+            return;
+        }
         if (posicionZ > carril-3)
         {
             posicionZ--;
         }
     }
     public void MoverLados(int cuanto){
+        grafico.eulerAngles = new Vector3(0,90*cuanto,0);
+        if(MirarAdelante())
+        {
+            return;
+        }
         lateral+=cuanto;
         lateral=Mathf.Clamp(lateral,-4,4);
+    }
+    
+    public bool MirarAdelante()
+    {
+        RaycastHit hit;
+        Ray rayo = new Ray(grafico.position + Vector3.up * 0.5f,grafico.forward);
+        if(Physics.Raycast(rayo,out hit,distanciaVista, capaObstaculos))
+        {
+            return true; 
+        }
+        return false;
     }
 }
