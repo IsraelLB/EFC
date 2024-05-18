1284785a41b194c0f97279c2ba1cf08e31a48b12
Mario Leal <mario.lealgarcia@alum.uca.es>
2024-05-18 23:02:38 +0200

    Sonidos de boton en menu principal

--- Assets/Script/MenuPrincipal.cs
+++ Assets/Script/MenuPrincipal.cs
@@ -2,19 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class MenuPrincipal : MonoBehaviour
 {
+    private AudioSource audioSource;
+    public AudioClip ClickBoton;
+    public Button startButton; 
+    public Button exitButton; 
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     public void OnStartButtonClicked()
     {
-        SceneManager.LoadScene("SampleScene");
+        startButton.interactable = false;
+        audioSource.PlayOneShot(ClickBoton);
+        StartCoroutine(LoadNextSceneAfterDelay("SampleScene", 0.9f));
     }
 
     public void OnExitButtonClicked()
     {
-        Application.Quit();
+        exitButton.interactable = false;
+        audioSource.PlayOneShot(ClickBoton);
+        StartCoroutine(QuitGameAfterDelay(1.0f));
         Debug.Log("hola");
 
+    }
+
+    IEnumerator LoadNextSceneAfterDelay(string sceneName, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(sceneName);
+    }
+
+    IEnumerator QuitGameAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Application.Quit();
+        startButton.interactable = true;
     }
 }
